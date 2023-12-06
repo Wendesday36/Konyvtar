a7c96b748fbdcb1bbb58caef3f0f1ba657e826ae
PappNiki20 <papp.nikoletta001@gmail.com>
2023-12-06 13:41:56 +0100

    termekek metodus keszen

--- src/konyvtarprogram/Konyvtar.java
+++ src/konyvtarprogram/Konyvtar.java
@@ -1,5 +1,26 @@
 package konyvtarprogram;
 
+import java.util.List;
+
 public class Konyvtar {
+     int keszlet;
+     int allapot;
+     List <AbstractTermek> termekek;
+    public Konyvtar(int keszlet, int allapot) {
+        this.keszlet = keszlet;
+        this.allapot = allapot;
+        
+    }
+  
+     public void termekek(){
+         Konyv k1 = new Konyv("Punk 57", "Penelope Dougles",0);
+         Manga m1 = new Manga("One Piece", "Ochiro Oda", "Ochiro Oda",1);
+         termekek.add(k1);
+         termekek.add(m1);
+         for (int i = 0; i < termekek.size(); i++) {
+             keszlet++;
+         }
+     }
+     
 
 }
