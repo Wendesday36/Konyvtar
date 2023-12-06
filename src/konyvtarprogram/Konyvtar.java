bfbdeb63c3d8fd5437def26ba0bfd4ddb253047b
PappNiki20 <papp.nikoletta001@gmail.com>
2023-12-06 12:30:41 +0100

    alap szerkezet

---
+++ src/konyvtarprogram/Konyvtar.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package konyvtarprogram;
+
+/**
+ *
+ * @author PappNikoletta(SZF_20
+ */
+public class Konyvtar {
+    
+}
